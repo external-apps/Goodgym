59f61493c25431ed016fd53baa9f1cdaed8275c4
ewelina <skibinska.ewelina@gmail.com>
2017-05-08 16:02:17 +0100

    Add baqsic routes
    related #15

--- src/server.js
+++ src/server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
 const app = express();
 
+app.set('view engine', 'hbs');
+
+app.engine('hbs', exphbs({
+  defaultLayout: path.join(__dirname, '/views/layouts/main.hbs')
+}));
+
+app.set('views', path.join(__dirname, '/views'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('home');
 });
 
 app.listen(3000, () => {
