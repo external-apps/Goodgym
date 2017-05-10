dfb0a33f8ab59a5e7f8b1540c073440adc5d3130
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-05-10 17:49:08 +0100

    Connect database to mLab!
    - add https server

--- src/database/db-connection.js
+++ src/database/db-connection.js
@@ -1,15 +1,33 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const conn = mongoose.connection;
 
-mongoose.connect('mongodb://peter:goodgym123-@cluster0-shard-00-00-reo2v.mongodb.net:27017,cluster0-shard-00-01-reo2v.mongodb.net:27017,cluster0-shard-00-02-reo2v.mongodb.net:27017/admin?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
+require('env2')(`${__dirname}/../../.env`);
+const user = process.env.DB_USER;
+const pass = process.env.DB_PASS;
+const database = process.env.DB_DATABASE;
+
+const uri = `mongodb://${user}:${pass}@ds137141.mlab.com:37141/${database}`;
+const options = {
+  server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+  replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }
+};
+
+mongoose.connect(uri, options);
+
+conn.on('error', console.error.bind(console, 'connection error:'));
+
+conn.once('open', function () {
+  console.log('We are connected!');
+});
 
 // create a schema
-let GoodGymSchema = new Schema({
+const GoodGymSchema = new Schema({
   run: String
 });
 
 // the schema is useless so far
 // we need to create a model using it
-let GoodGymDB = mongoose.model('GoodGymDB', GoodGymSchema);
+const GoodGymDB = mongoose.model('GoodGymDB', GoodGymSchema);
 
 module.exports = GoodGymDB;
--- src/database/db-driver.js
+++
@@ -1,8 +0,0 @@
-const MongoClient = require('mongodb').MongoClient;
-
-const uri = 'mongodb://peter:goodgym123-@cluster0-shard-00-00-reo2v.mongodb.net:27017,cluster0-shard-00-01-reo2v.mongodb.net:27017,cluster0-shard-00-02-reo2v.mongodb.net:27017/admin?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
-MongoClient.connect(uri, (err, db) => {
-  if (err) { console.log(err); }
-  console.log('We are connected');
-  db.close();
-});
