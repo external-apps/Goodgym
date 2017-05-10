5a9e5f0a18950232034b50244e87d7f693ce2571
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-05-10 15:27:51 +0100

    First attempt at connecting mongoose to atlas.
    Related #25

--- src/database/db-connection.js
+++ src/database/db-connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://localhost/goodgym');
+mongoose.connect('mongodb://peter:goodgym123-@cluster0-shard-00-00-reo2v.mongodb.net:27017,cluster0-shard-00-01-reo2v.mongodb.net:27017,cluster0-shard-00-02-reo2v.mongodb.net:27017/admin?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
 
 // create a schema
 let GoodGymSchema = new Schema({
