86d0eeb9216c50a6c3cad45867501abf69f0cad4
ewelina <skibinska.ewelina@gmail.com>
2017-05-12 17:10:30 +0100

    Add qr code and once it scan redirect to app

--- src/routes/qr.js
+++ src/routes/qr.js
@@ -1,38 +1,5 @@
-const fs = require('fs');
-require('env2')(`${__dirname}/../../.env`);
-
-const YotiClient = require('yoti-node-sdk');
-const SDK_ID = process.env.CLIENT_SDK_ID;
-const PEM_PATH = `${__dirname}/../keys/GoodGym-access-security.pem`;
-const PEM = fs.readFileSync(PEM_PATH);
-
-let yotiClient = new YotiClient(SDK_ID, PEM);
-
 const qr = (req, res) => {
-  let token = req.query.token;
-  if (!token) {
-    res.render('home', {
-      error: 'No token has been provided.'
-    });
-    return;
-  }
-  let promise = yotiClient.getActivityDetails(token).then((activityDetails) => {
-    res.render('task-sheet', {
-      userId: activityDetails.getUserId(),
-      profile: activityDetails.getUserProfile(),
-      outcome: activityDetails.getOutcome()
-    });
-  }).catch((err) => {
-    console.error(err);
-  });
-  console.log(token);
   res.render('qr');
 };
 
 module.exports = qr;
-
-// const qr = (req, res) => {
-//   res.render('qr');
-// };
-//
-// module.exports = qr;
--- src/routes/tasksheet.js
+++ src/routes/tasksheet.js
@@ -1,5 +1,31 @@
+const fs = require('fs');
+require('env2')(`${__dirname}/../../.env`);
+
+const YotiClient = require('yoti-node-sdk');
+const SDK_ID = process.env.CLIENT_SDK_ID;
+const PEM_PATH = `${__dirname}/../keys/GoodGym-access-security.pem`;
+const PEM = fs.readFileSync(PEM_PATH);
+
+let yotiClient = new YotiClient(SDK_ID, PEM);
+
 const tasksheet = (req, res) => {
-  res.render('task-sheet');
+  let token = req.query.token;
+  if (!token) {
+    return res.send('No token found');
+  }
+  yotiClient.getActivityDetails(token).then((activityDetails) => {
+    const user = {
+      userId: activityDetails.getUserId(),
+      profile: activityDetails.getUserProfile(),
+      outcome: activityDetails.getOutcome()
+    };
+    res.render('task-sheet', {
+      user: user.profile.givenNames
+    });
+  }).catch((err) => {
+    console.error(err);
+  });
+  console.log(token);
 };
 
 module.exports = tasksheet;
