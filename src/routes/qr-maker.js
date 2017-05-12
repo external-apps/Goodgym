4b5e4de1f7158936439a37bc4a9a5c11e14a235c
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-05-12 17:11:27 +0100

    Display Yoti QR on wr route!
    Code taken from J.Whiles Scouts app.
    
    Related #29

---
+++ src/routes/qr-maker.js
@@ -0,0 +1,26 @@
+const request = require('request');
+const QRCode = require('qrcode-svg');
+
+const qrMaker = (req, res) => {
+  request.get(`https://www.yoti.com/connect/493d7947-896e-49f6-afd4-e6136b8a9a34`, (e, response, body) => {
+    // Get URL
+    const url = body.match(/https:\/\/code\.yoti\.com\/.*\?/)[0].slice(0, -1);
+    // Get proto
+    const proto = body.match(/proto_.*=/)[0];
+    // Make SVG
+    const svg = new QRCode({
+      content: url,
+      color: 'white',
+      background: '#432668',
+      width: 300,
+      height: 300,
+      padding: 4
+    }).svg();
+    // Give to client
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({svg, proto, url}));
+    // res.render(JSON.stringify({svg, proto, url}));
+  });
+};
+
+module.exports = qrMaker;
--- src/routes/qr.js
+++ src/routes/qr.js
@@ -1,12 +1,20 @@
-const fs = require('fs');
-require('env2')(`${__dirname}/../../.env`);
+// const fs = require('fs');
+// require('env2')(`${__dirname}/../../.env`);
 
-const YotiClient = require('yoti-node-sdk');
-const SDK_ID = process.env.CLIENT_SDK_ID;
-const PEM_PATH = `${__dirname}/../keys/GoodGym-access-security.pem`;
-const PEM = fs.readFileSync(PEM_PATH);
+// const YotiClient = require('yoti-node-sdk');
+// const SDK_ID = process.env.CLIENT_SDK_ID;
+// const PEM_PATH = `${__dirname}/../keys/GoodGym-access-security.pem`;
+// const PEM = fs.readFileSync(PEM_PATH);
+//
+// let client = new YotiClient(SDK_ID, PEM);
+//
+// const qr = (req, res) => {
+//   res.render('qr');
+// };
+//
+// module.exports = qr;
 
-let yotiClient = new YotiClient(SDK_ID, PEM);
+// ------
 
 const qr = (req, res) => {
   res.render('qr');
