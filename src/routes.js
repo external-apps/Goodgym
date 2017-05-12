9c1f9d24e825623d0e7c8a56fa498b436d054771
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-05-12 17:11:27 +0100

    Display Yoti QR on wr route!
    Code taken from J.Whiles Scouts app.
    
    Related #29

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const home = require('./routes/home');
 const qr = require('./routes/qr');
 const tasksheet = require('./routes/tasksheet');
+const qrMaker = require('./routes/qr-maker');
 
 router.get('/:id', home);
 router.get('/qr/:id', qr);
 router.get('/task-sheet/:id', tasksheet);
+router.get('/qr-maker/:id', qrMaker);
 
 module.exports = router;
