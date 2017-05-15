2b1eef444f98babc6493bcc8694acc2ef55f9002
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-05-15 10:33:38 +0100

    Refactor index.js
    Remove unneeded if statement.

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
-'use strict';
+(function () {
+  'use strict';
 
-if (window.location.pathname.match(/^\/[0-9a-z]+$/)) {
   var registerButton = document.getElementsByClassName('_yoti-verify-button')[0];
   registerButton.href = window.location.origin + '/qr/test';
-}
+})();
