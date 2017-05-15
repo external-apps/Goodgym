2a3eae314916bd14d86f5a185ca9f4a1960df810
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-05-15 17:58:24 +0100

    Create constructor function for the Task Sheet information.
    Related #37

--- public/index.js
+++ public/index.js
@@ -5,4 +5,26 @@
   if (registerButton) {
     registerButton.href = window.location.origin + '/qr/test';
   }
+
+  var saveButton = document.getElementsByClassName('save-button')[0];
+  saveButton.addEventListener('click', saveToDatabase);
+
+  function Task () {
+    this.task = arguments[0][0];
+    this.location = arguments[0][1];
+    this.purpose = arguments[0][2];
+    this.contact = arguments[0][3];
+    this.risk = arguments[0][4];
+  }
+
+  function saveToDatabase () {
+    // Send post request to backend with the data from all of the text areas.
+    var taskInfoArray = [].slice.call(document.querySelectorAll('textarea'));
+    var taskInfoObj = taskInfoArray.map(function (textarea) {
+      return textarea.value;
+    });
+
+    var taskObj = new Task(taskInfoObj);
+    console.log(taskObj);
+  }
 })();
