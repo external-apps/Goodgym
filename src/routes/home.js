30a6801edcce43567d27f68adbb5e35a74ace9ac
ewelina <skibinska.ewelina@gmail.com>
2017-05-19 17:03:47 +0100

    Update home route
    realted #6

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,5 +1,14 @@
+const getRun = require('./getRunFromGoodGym');
+
 const home = (req, res) => {
-  res.render('home');
+  const paramId = req.params.id;
+  if (paramId !== 'favicon.ico') {
+    getRun(paramId, (err, run) => {
+      if (err) console.error(err);
+      console.log(run, 'run from home');
+      res.render('home', run);
+    });
+  }
 };
 
 module.exports = home;
