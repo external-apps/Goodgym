e84555be08405728f1f59f693dbc10a73ad72443
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-05-24 15:11:36 +0100

    Return the name from Yoti after sucsessfull data transfer.
    Related #54

--- src/routes/confirmation.js
+++ src/routes/confirmation.js
@@ -1,5 +1,33 @@
+const fs = require('fs');
+require('env2')(`${__dirname}/../../.env`);
+
+const YotiClient = require('yoti-node-sdk');
+const SDK_ID = process.env.CLIENT_SDK_ID;
+const PEM_PATH = `${__dirname}/../keys/GoodGym-access-security.pem`;
+const PEM = fs.readFileSync(PEM_PATH);
+
+let yotiClient = new YotiClient(SDK_ID, PEM);
+
+if (!SDK_ID) {
+  console.log('Enviroment variable CLIENT_SDK_ID must be set.');
+}
+
 const confirmation = (req, res) => {
-  res.render('confirmation');
+  const token = req.query.token;
+  if (!token) {
+    res.status(500).send('No yoti token provided');
+    return;
+  }
+  yotiClient.getActivityDetails(token).then((activityDetails) => {
+    const userProfile = activityDetails.getUserProfile();
+    res.render('confirmation', {
+      firstName: userProfile.givenNames,
+      lastName: userProfile.familyName
+    });
+  }).catch((err) => {
+    console.error(err);
+  });
+  console.log(token);
 };
 
 module.exports = confirmation;
