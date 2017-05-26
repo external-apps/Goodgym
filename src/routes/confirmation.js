6b8e4b4e862819305b47154ec7d82422a316cfc2
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-05-26 12:09:03 +0100

    Add security.pem to environment variables

--- src/routes/confirmation.js
+++ src/routes/confirmation.js
@@ -3,8 +3,8 @@ require('env2')(`${__dirname}/../../.env`);
 
 const YotiClient = require('yoti-node-sdk');
 const SDK_ID = process.env.CLIENT_SDK_ID;
-const PEM_PATH = `${__dirname}/../keys/GoodGym-access-security.pem`;
-const PEM = fs.readFileSync(PEM_PATH);
+const PEM = process.env.SECURITY_PEM;
+// const PEM = fs.readFileSync(PEM_PATH);
 
 let yotiClient = new YotiClient(SDK_ID, PEM);
 
--- src/server-start.js
+++ src/server-start.js
@@ -1,8 +1,5 @@
 const server = require('./server.js');
-const https = require('https');
 const http = require('http');
-const fs = require('fs');
-const path = require('path');
 
 const port = process.env.PORT || 3000;
 
