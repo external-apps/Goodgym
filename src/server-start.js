6232210add34b83f1bf5c670f72e436e65d745d0
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-05-26 11:58:36 +0100

    Remove https server.
    Heroku creates a https server.
    
    Related #14

--- src/server-start.js
+++ src/server-start.js
@@ -4,17 +4,8 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const key = fs.readFileSync(path.join(__dirname, '/keys/key.pem'));
-const cert = fs.readFileSync(path.join(__dirname, '/keys/cert.pem'));
 const port = process.env.PORT || 3000;
 
-http.createServer(server);
-
-if (!module.parent) {
-  https.createServer({
-    key: key,
-    cert: cert
-  }, server).listen(port, () => {
-    console.log('Listening on port ' + port);
-  });
-}
+http.createServer(server).listen(port, () => {
+  console.log('Listening on port ' + port);
+});
