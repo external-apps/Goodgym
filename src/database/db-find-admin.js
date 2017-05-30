fd2028612835d6974105c1d454b957fc3e76495b
ewelina <skibinska.ewelina@gmail.com>
2017-05-30 12:00:05 +0100

    Add returns to db-find-admin.js
    releated #51

--- src/database/db-find-admin.js
+++ src/database/db-find-admin.js
@@ -1,16 +1,16 @@
 const { Admin } = require('./db-connection');
 
-Admin.findOne({username: 'admin', password: 'admin123'}, (err, admin) => {
+Admin.findOne({username: 'admin1', password: 'admin123'}, (err, admin) => {
   if (err) {
-    console.log(err);
+    return console.log(err);
     // return res.status(500).send();
   }
 
   if (!admin) {
-    console.log('There is no admin');
+    return console.log('There is no admin');
     // return res.status(404).send();
   }
-  console.log(admin, 'admin');
+  return console.log(admin, 'admin');
   // req.session.admin = admin;
   // return res.status(200).send();
 });
