4aaa0c9316dd5fd80d55fe9184a5c7e66f236fd8
ewelina <skibinska.ewelina@gmail.com>
2017-05-30 12:57:07 +0100

    Add express-session
    releated #51

--- src/routes/post-login.js
+++ src/routes/post-login.js
@@ -6,17 +6,17 @@ const postLogin = (req, res) => {
   console.log(req.session, 'req.session');
   Admin.findOne({username: username, password: password}, (err, admin) => {
     if (err) {
-      return console.log(err);
-      // return res.status(500).send();
+      console.log(err);
+      return res.status(500).send('POST request not made!');
     }
 
     if (!admin) {
-      return console.log('There is no admin');
-      // return res.status(404).send();
+      console.log('There is no admin');
+      return res.status(404).send('You need to be logged in to see this page');
     }
-    return console.log(admin, 'admin');
+    console.log(admin, 'admin');
     // req.session.admin = admin;
-    // return res.status(200).send();
+    return res.status(200).send('Hello admin!');
   });
 };
 
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 const path = require('path');
@@ -15,6 +16,7 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '/templates/views'));
 
 app.use(bodyParser());
+app.use(session({ secret: 'shhsecret' }));
 app.use(express.static('public'));
 app.use('/', routes);
 
