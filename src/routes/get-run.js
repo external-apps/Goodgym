a13156bd093bbff083cd252026c6ffd598d6bd0f
Peter Rhodes <rhodespeter@hotmail.co.uk>
2017-05-30 10:59:31 +0100

    Respond with error view when no run found in database
    Fixes #56

--- src/routes/get-run.js
+++ src/routes/get-run.js
@@ -3,7 +3,11 @@ const findOneRun = require('./../database/db-find-one');
 const getRun = (req, res) => {
   const runId = req.params.id;
   findOneRun(runId, (err, run) => {
-    if (err) console.error(err);
+    if (err) {
+      console.error(err);
+      res.render('error', null);
+      return;
+    }
     res.json(run);
   });
 };
