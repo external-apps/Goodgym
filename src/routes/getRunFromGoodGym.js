55d3b7ea6bc398ca9d226dc0731f07d375ff30b1
ewelina <skibinska.ewelina@gmail.com>
2017-06-01 15:17:51 +0100

    Clean up
    releated #51

--- src/routes/getRunFromGoodGym.js
+++ src/routes/getRunFromGoodGym.js
@@ -1,10 +1,9 @@
 const request = require('request');
 
 const getRunFromGoodGym = (paramId, cb) => {
-  request('https://goodgym-staging-pr-646.herokuapp.com/api/happenings/' + paramId, (err, res, body) => {
+  request(`https://goodgym-staging-pr-646.herokuapp.com/api/happenings/${paramId}`, (err, res, body) => {
     if (err) return cb(err);
     if (!err && res.statusCode === 200) {
-      console.log(JSON.parse(body));
       return cb(null, JSON.parse(body).item);
     }
   });
