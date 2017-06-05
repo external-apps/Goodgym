8786c2c577d4ce9de007c413ee0e1a0702243be6
ewelina <skibinska.ewelina@gmail.com>
2017-06-05 16:10:47 +0100

    Update queries to reflect new Run schema
    related #64

--- src/database/db-find-one.js
+++ src/database/db-find-one.js
@@ -1,7 +1,7 @@
 const Run = require('./db-connection');
 
 const findOneRun = (runId, cb) => {
-  Run.find({'run.runId': runId}, (err, run) => {
+  Run.find({runId: runId}, (err, run) => {
     if (err) return cb(err);
     cb(null, run);
   });
--- src/database/db-update-run.js
+++ src/database/db-update-run.js
@@ -3,16 +3,16 @@ const Run = require('./db-connection');
 const updateRun = (inputRun) => {
   const updateOption = {
     '$set': {
-      'run.task': inputRun.task,
-      'run.location': inputRun.location,
-      'run.purpose': inputRun.purpose,
-      'run.contact': inputRun.contact,
-      'run.risk': inputRun.risk,
-      'run.email': inputRun.email
+      task: inputRun.task,
+      location: inputRun.location,
+      purpose: inputRun.purpose,
+      contact: inputRun.contact,
+      risk: inputRun.risk,
+      email: inputRun.email
     }
   };
 
-  Run.findOneAndUpdate({'run.runId': inputRun.runId}, updateOption, (err, run) => {
+  Run.findOneAndUpdate({runId: inputRun.runId}, updateOption, (err, run) => {
     if (err) throw err;
     console.log(run, 'Updated run!');
   });
