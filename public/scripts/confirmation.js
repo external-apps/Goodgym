2108168ce2f380c62552f128504a7514b586dc55
ewelina <skibinska.ewelina@gmail.com>
2017-06-08 16:31:02 +0100

    Change getElementsByClassName to querySelector in confirmation.js
    related #65

--- public/scripts/confirmation.js
+++ public/scripts/confirmation.js
@@ -3,7 +3,7 @@
 (function () {
   'use strict';
 
-  var backToQRButton = document.getElementsByClassName('button-container__confirmation-button')[0];
+  var backToQRButton = document.querySelector('.button-container__confirmation-button');
   backToQRButton.addEventListener('click', function () {
     window.location.pathname = '/qr/' + sessionStorage.run_id;
   });
@@ -11,7 +11,7 @@
   var taskSheetLocation = window.location.origin + '/task-sheet/' + sessionStorage.run_id;
   index.httpPostRequest({
     taskSheetURL: taskSheetLocation,
-    firstName: document.getElementsByClassName('confirmation-body__firstName')[0].innerText,
-    emailAddress: document.getElementsByClassName('confirmation-body__emailAddress')[0].innerText
+    firstName: document.querySelector('.confirmation-body__firstName').innerText,
+    emailAddress: document.querySelector('.confirmation-body__emailAddress').innerText
   }, '/send-task-sheet/:id');
 })();
