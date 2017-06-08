2b9edfda786cb97b53a23f95803c5b24f806e3fe
ewelina <skibinska.ewelina@gmail.com>
2017-06-08 18:51:11 +0100

    Clean up
    related #95

--- src/routes/tasksheet.js
+++ src/routes/tasksheet.js
@@ -1,20 +1,3 @@
-// const getRun = require('../helpers/get-run-from-goodgym');
-
-// const taskSheet = (req, res) => {
-//   const paramId = req.params.id;
-//   if (paramId !== 'favicon.ico') {
-//     getRun(paramId, (err, run) => {
-//       if (err) {
-//         console.error(err);
-//         res.render('error', { error: 'No run found in our database!' });
-//         return;
-//       }
-//       res.render('task-sheet', {
-//         run: run
-//       });
-//     });
-//   }
-// };
 const apiKey = process.env.API_KEY;
 
 const taskSheet = (req, res) => {
